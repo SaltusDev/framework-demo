3fefb03a3353af6e9a6ee547825e3b268200c640
Pedro de Carvalho <p@43.lc>
2019-04-24 00:02:27 +0100

    Add demo models

---
+++ src/models/post-type-basic.php
@@ -0,0 +1,15 @@
+<?php
+return [
+	'type'     => 'cpt',
+	'name'     => 'book',
+	'supports' => [
+		'title',
+		'editor',
+		'thumbnail',
+	],
+	'labels'   => [
+		'has_one'     => 'Book',
+		'has_many'    => 'Books',
+		'text_domain' => 'saltus',
+	],
+];
