d3a2d9172d4040959f3596c1a4c3ba9f978cccb1
Pedro de Carvalho <p@43.lc>
2019-06-13 18:27:11 +0100

    Add meta fields to demo cpt

--- src/models/post-type-basic.php
+++ src/models/post-type-basic.php
@@ -12,4 +12,31 @@ return [
 		'has_many'    => 'Movies',
 		'text_domain' => 'saltus',
 	],
+	/**
+	* reserved words:
+	* - meta
+	* - section
+	* - fields
+	*/
+	'meta'     => [
+		'section' => [
+			'id'     => 'aaaa',
+			'title'  => 'title1',
+			'fields' => [
+				// 'id' => array of settings
+				'subtitle' => [
+					'type'        => 'text',
+					'name'        => __( 'Subtitle', 'framework-demo' ),
+					'description' => __( 'test!', 'framework-demo' ),
+					'default'     => '',
+				],
+				'lead'     => [
+					'type'        => 'text',
+					'name'        => __( 'Lead', 'framework-demo' ),
+					'description' => __( 'A longer paragraph.', 'framework-demo' ),
+					'default'     => '',
+				],
+			],
+		],
+	],
 ];
