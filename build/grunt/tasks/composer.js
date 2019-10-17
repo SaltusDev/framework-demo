c9324a960a70a205fa468b8bbaff90b780cfdd3c
Pedro de Carvalho <p@43.lc>
2019-10-17 23:42:43 +0100

    Add composer autoload optimization tasks

--- build/grunt/tasks/composer.js
+++ build/grunt/tasks/composer.js
@@ -1,5 +1,13 @@
 module.exports = function(grunt) {
 
+	grunt.config( ['composer'], {
+		release: {
+			options: {
+				flags: ['no-dev' ],
+			},
+		},
+	});
+
 	grunt.loadNpmTasks('grunt-composer');
 
 };
