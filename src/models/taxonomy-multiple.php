554c4adf0e3ac125430581975e045685cd10a2e4
carmoreira <carlostrickster@gmail.com>
2020-05-19 23:15:01 +0100

    more taxonomy examples

--- src/models/taxonomy-multiple.php
+++ src/models/taxonomy-multiple.php
@@ -14,7 +14,23 @@ return [
 	],
 	[
 		'type'         => 'tag',
-		'name'         => 'mood',
+		'name'         => 'country',
+		'labels'       => [
+			'has_one'  => 'Country',
+			'has_many' => 'Countries',
+			'overrides' => [
+				'labels' => [
+					'menu_name' => 'Places',
+				]
+			],
+		],
+		// args - third parameter for register_taxonomy
+		'options' => [
+			'description'  => 'Description for this taxonomy',
+			'public'       => true,
+			'show_in_menu' => true,
+		],
+		// object_type - second parameter for register_taxonomy
 		'associations' => [
 			'book',
 			'post',
