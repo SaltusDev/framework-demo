0291b95722d4b02686b36ec5deb7a2449b084afe
Pedro de Carvalho <p@43.lc>
2026-06-20 22:35:34 +0800

    style(models): migrate from short to long array syntax

--- src/models/post-type-basic.php
+++ src/models/post-type-basic.php
@@ -1,17 +1,17 @@
 <?php
 
-return [
+return array(
 	'type'     => 'cpt',
 	'name'     => 'movie',
-	'supports' => [
+	'supports' => array(
 		'title',
 		'editor',
 		'thumbnail',
 		'page-attributes',
-	],
-	'labels'   => [
+	),
+	'labels'   => array(
 		'has_one'        => 'Movie',
 		'has_many'       => 'Movies',
 		'featured_image' => 'Movie Poster',
-	],
-];
+	),
+);
--- src/models/taxonomy-multiple.php
+++ src/models/taxonomy-multiple.php
@@ -1,39 +1,39 @@
 <?php
-return [
-	[
+return array(
+	array(
 		'type'         => 'category',
 		'name'         => 'genre',
-		'associations' => [
+		'associations' => array(
 			'book',
-		],
-	],
-	[
+		),
+	),
+	array(
 		'type'         => 'category',
 		'name'         => 'writer',
 		'associations' => 'book',
-	],
-	[
+	),
+	array(
 		'type'         => 'tag',
 		'name'         => 'country',
-		'labels'       => [
-			'has_one'  => 'Country',
-			'has_many' => 'Countries',
-			'overrides' => [
-				'labels' => [
+		'labels'       => array(
+			'has_one'   => 'Country',
+			'has_many'  => 'Countries',
+			'overrides' => array(
+				'labels' => array(
 					'menu_name' => 'Places',
-				]
-			],
-		],
+				),
+			),
+		),
 		// args - third parameter for register_taxonomy
-		'options' => [
+		'options'      => array(
 			'description'  => 'Description for this taxonomy',
 			'public'       => true,
 			'show_in_menu' => true,
-		],
+		),
 		// object_type - second parameter for register_taxonomy
-		'associations' => [
+		'associations' => array(
 			'book',
 			'post',
-		],
-	],
-];
+		),
+	),
+);
