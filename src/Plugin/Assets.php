e97c6d93e6b22a946e2591428d5c9c0f00fea181
Pedro de Carvalho <p@43.lc>
2026-07-04 16:13:10 +0800

    fix(assets): use dynamic slug for admin style hook

--- src/Plugin/Assets.php
+++ src/Plugin/Assets.php
@@ -37,7 +37,7 @@ class Assets {
 	 *
 	 */
 	public function load_admin_styles( string $hook_suffix = '' ): void {
-		if ( ! $hook_suffix || ! str_contains( $hook_suffix, 'framework-demo' ) ) {
+		if ( ! $hook_suffix || ! str_contains( $hook_suffix, $this->core->get_name() ) ) {
 			return;
 		}
 
